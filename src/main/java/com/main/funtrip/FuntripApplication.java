ee809b792ee0a5438897ad13e8c81ecf62f384cd
baegsihun <qorwlgml2003@naver.com>
2023-10-14 21:54:11 +0900

    push TEST

--- src/main/java/com/main/funtrip/FuntripApplication.java
+++ src/main/java/com/main/funtrip/FuntripApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FuntripApplication {
-
+    /** push TEST    **/
     public static void main(String[] args) {
         SpringApplication.run(FuntripApplication.class, args);
     }
