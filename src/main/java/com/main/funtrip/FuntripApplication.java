1631f5c8a644b9b76a27731503372b4ce535a58f
jbkim4040 <jbkim404037@gmail.com>
2023-10-14 21:09:57 +0900

    funtrip 백엔드 개발 시작!!

---
+++ src/main/java/com/main/funtrip/FuntripApplication.java
@@ -0,0 +1,13 @@
+package com.main.funtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuntripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuntripApplication.class, args);
+    }
+
+}
