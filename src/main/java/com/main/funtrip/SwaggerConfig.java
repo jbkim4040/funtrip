e8e9765d7cb43ae5ac14728d8cb544f0fe40973f
baegsihun <qorwlgml2003@naver.com>
2023-10-19 21:17:02 +0900

    스웨거 설정

---
+++ src/main/java/com/main/funtrip/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.main.funtrip;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    //localhost:8090/swagger-ui.html#/
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+//                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.main.funtrip"))
+                // 스웨거가 RestController를 전부 스캔을 한다.
+                // basePackage => 어디를 범위로 스캔을 할 것인지 작성
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("test1")
+                .description("test2")
+                .version("3.0")
+                .build();
+    }
+
+}
---
+++ src/main/java/com/main/funtrip/test/controller/testController.java
@@ -0,0 +1,16 @@
+package com.main.funtrip.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class testController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
