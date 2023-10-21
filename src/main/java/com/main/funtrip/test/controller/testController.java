2ab335498bc7cacdcb97cdd91799c982f2fedfd9
sihun93 <82190707+sihun93@users.noreply.github.com>
2023-10-22 07:21:58 +0900

    서버연결 너가 안올렸잖아!!!!!

--- src/main/java/com/main/funtrip/test/controller/testController.java
+++ src/main/java/com/main/funtrip/test/controller/testController.java
@@ -1,16 +1,27 @@
 package com.main.funtrip.test.controller;
 
+import com.main.funtrip.common.config.StartupApplicationListenerConfig;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/test")
 public class testController {
-
+    private static final Logger LOG = Logger.getLogger(String.valueOf(testController.class));
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
 
+    @GetMapping("/test")
+    public List<String> test(){
+        LOG.info("test11111");
+        return Arrays.asList("서버", "연결");
+    }
 }
